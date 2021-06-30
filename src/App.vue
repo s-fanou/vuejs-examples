9d19ad799b3853a520c9f0226a657d4ac0c3eb48
Sotiris Fanou <sotiris.fanou@gmail.com>
2021-06-30 11:24:57 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <h1>{{ msg }}</h1>
+
+    <sot-message> This is my message </sot-message>
+    
+  </div>
+</template>
+
+<script>
+
+import SotMessage from './components/Message';
+
+export default {
+  name: 'app',
+  components: { SotMessage },
+  data () {
+    return {
+      msg: 'Sot-App'
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/components/Message.vue
@@ -0,0 +1,19 @@
+ <template>
+  <div><slot></slot></div>
+</template>
+
+<script>
+export default {
+  name: 'SotMessage',
+  components: {},
+  props: {},
+  data() {
+    return {}
+  },
+  mounted() {},
+  beforeDestroy() {},
+  methods: {}
+}
+</script>
+
+<style lang="css" scoped></style>
